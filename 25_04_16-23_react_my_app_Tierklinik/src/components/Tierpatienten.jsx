bfed3f47979e267e3a8d911756a753c513368774
Macky232323 <a.manhardt@mackalex.de>
2025-04-23 17:19:11 +0200

    Navbar um den Button Tierpatienten erweitert

---
+++ 25_04_16-23_react_my_app_Tierklinik/src/components/Tierpatienten.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+ 
+
+ function Tierpatienten() {
+  return (
+  <div>
+  <h1>Tierpatienten</h1>
+  {/* Hier kommt der Inhalt für die Tierpatienten-Seite hin */}
+  </div>
+  );
+ }
+ 
+
+ export default Tierpatienten;
--- 25_04_16-23_react_my_app_Tierklinik/src/components/Titel.jsx
+++ 25_04_16-23_react_my_app_Tierklinik/src/components/Titel.jsx
@@ -1,25 +1,29 @@
 import React from 'react';
  import { NavLink } from 'react-router-dom';
  import './Titel.css';
- import './NavBar.css'; // Importiere die CSS-Datei für die Navigationsleiste
+ import './NavBar.css';
  import bg_hell from '../assets/bg_hell.jpg';
  import bg_dunkel from '../assets/bg_dunkel.jpg';
+ 
 
  function Titel({ darkMode }) {
   const backgroundImage = darkMode ? bg_dunkel : bg_hell;
+ 
 
   return (
   <div className={`titel-container ${darkMode ? 'dark-mode' : 'light-mode'}`} style={{ backgroundImage: `url(${backgroundImage})` }}>
   <nav className="nav-bar">
   <NavLink exact to="/" className="nav-button" activeClassName="active">Home</NavLink>
+  <NavLink to="/tierpatienten" className="nav-button" activeClassName="active">Tierpatienten</NavLink>
+  <NavLink to="/about" className="nav-button" activeClassName="active">About</NavLink>
   <NavLink to="/kontakt" className="nav-button" activeClassName="active">Kontakt</NavLink>
   <NavLink to="/faq" className="nav-button" activeClassName="active">FAQ</NavLink>
-  <NavLink to="/about" className="nav-button" activeClassName="active">About</NavLink>
   <NavLink to="/impressum" className="nav-button" activeClassName="active">Impressum</NavLink>
   </nav>
   <h1>Tierklinik-Techstarter</h1>
   </div>
   );
  }
+ 
 
  export default Titel;
