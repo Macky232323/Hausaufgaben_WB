a5276eaf1bbfa7fa99e5d8040d30ea352c385c92
Macky232323 <a.manhardt@mackalex.de>
2025-04-25 10:28:14 +0200

    Alter gegen age getauscht

--- 25_04_16-24_react_my_app_Tierklinik/src/components/TierCard.jsx
+++ 25_04_16-24_react_my_app_Tierklinik/src/components/TierCard.jsx
@@ -1,25 +1,22 @@
 import React from 'react';
- import { useNavigate } from 'react-router-dom';
- import './TierCard.css';
- 
+import { useNavigate } from 'react-router-dom';
+import './TierCard.css';
 
- function TierCard({ name, art, krankheit, alter, gewicht, bild, id }) {
-  const navigate = useNavigate();
- 
+function TierCard({ name, art, krankheit, age, gewicht, bild, id }) {
+ const navigate = useNavigate();
 
-  return (
-  <div className="tier-card" onClick={() => navigate(`/tiere/${id}`)}>
-  <img src={`/assets/${bild}`} alt={name} className="tier-bild" />
-  <div className="tier-info">
-  <h3>{name}</h3>
-  <p>Art: {art}</p>
-  <p>Krankheit: {krankheit}</p>
-  <p>Alter: {alter}</p>
-  <p>Gewicht: {gewicht}</p>
-  </div>
-  </div>
-  );
- }
- 
+ return (
+   <div className="tier-card" onClick={() => navigate(`/tiere/${id}`)}>
+     <img src={`/assets/${bild}`} alt={name} className="tier-bild" />
+     <div className="tier-info">
+       <h3>{name}</h3>
+       <p>Art: {art}</p>
+       <p>Krankheit: {krankheit}</p>
+       <p>Alter: {age}</p>
+       <p>Gewicht: {gewicht}</p>
+     </div>
+   </div>
+ );
+}
 
- export default TierCard;
+export default TierCard;
