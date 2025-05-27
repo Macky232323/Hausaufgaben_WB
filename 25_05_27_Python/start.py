381cbac2b58b5a467f85053b32861107709f4d01
Macky232323 <a.manhardt@mackalex.de>
2025-05-27 10:45:35 +0200

    .

--- 25_05_27_Python/start.py
+++ 25_05_27_Python/start.py
@@ -1,9 +1,25 @@
 # Snake Case Schreibweise
-first_name = "Alexander"
+first_name = "Alexander" # Variable Namen
 last_name = "Manhardt"
-money = 1234567
-gewicht = 71.7
-hat_einen_bart = True
-
+money = 1234567 # integer
+gewicht = 71.7 # float
+hat_einen_bart = True #False # boolean
 
 print(first_name, last_name, gewicht)
+
+a = 5 
+b = 7
+
+print(a + b) # Addition
+print(a - b) # Subtraktion
+print(a / b) # Division
+print(a * b) # Multiplikation
+
+print(a % b) # modulu Rest der Division
+
+print(2**4) # Potenzierung 2 hoch4
+print(a // b) # Ganzzahldivision
+
+print("Hallo " + first_name + " " + last_name)
+print(3* (first_name + " " + last_name + " ")) # String Multiplikation
+print(f"Hallo {first_name} {last_name}") # f-String
